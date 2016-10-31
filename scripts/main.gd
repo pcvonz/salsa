8ff8198dd7a67aa3d147283ac2b0d42018e9657a
Paul Von Zimmerman <pcvonz@gmail.com>
2016-10-30 23:36:31 -0700

    Adding start screen. Can pick 1 or 2 player

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,10 +4,15 @@ extends Node2D
 # member variables here, example:
 # var a=2
 # var b="textvar"
-export var player_count = 1
-
+var start_pos = 100
 func _ready():
+	var player_count = get_node("/root/global").player_count
+	var player = preload('res://scenes/player.tscn').instance()
 	for i in range(0, player_count):
-		pass
+		var player_duplicate = player.duplicate()
+		player_duplicate.set_pos(Vector2(start_pos, player_duplicate.get_pos().y))
+		start_pos += 100
+		call_deferred("add_child", player_duplicate)
+		
 
 
