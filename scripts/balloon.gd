aa69bccfce3690288b9eea4e68ec8742f75d3c3f
pcvonz <pcvonz@gmail.com>
2016-11-02 13:38:43 -0700

    Added ability to make balloons go back and forth

--- scripts/balloon.gd
+++ scripts/balloon.gd
@@ -1,12 +1,40 @@
-
+tool
 extends Area2D
 
-func floating_away():
+export(bool) var moving = false setget set_move
+export var moving_dist = 2
+export var period = .5
+var time_elapsed = 0
+var floating_away = false
+var original_pos = get_pos()
+
+
+func set_move(is_moving):
+	if(is_moving == true):
+		original_pos = get_pos()
+	else:
+		set_pos(original_pos)
+	moving = is_moving
+	time_elapsed = 0
+	set_fixed_process(is_moving)
+
+func _ready():
 	set_fixed_process(true)
+	if !(get_tree().is_editor_hint()):
+		connect("body_enter", self, "drop_energy")
+
+func floating_away():
+	floating_away = true
 
 func _fixed_process(delta):
 	#If process is set to true, then balloon floats away
-	set_pos(Vector2(get_pos().x, get_pos().y - 2))
+	if(floating_away == true):
+		set_pos(Vector2(get_pos().x, get_pos().y - 2))
+	time_elapsed += delta
+	if moving == true:
+		var move_to = Vector2(moving_dist*sin(period * time_elapsed), 0)
+		set_pos(get_pos() + move_to)
+		
 
 func drop_energy(body):
 	get_node('Sprite').queue_free()
@@ -16,7 +44,5 @@ func drop_energy(body):
 	if(get_node('energy')):
 		get_node('energy').falling = true
 
-func _ready():
-	connect("body_enter", self, "drop_energy")
 
 
