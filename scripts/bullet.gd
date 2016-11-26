499324551a96db2e86304e4d32aa93597755c4e0
Crifen <connor.rifenbery@gmail.com>
2016-11-25 19:33:05 -0800

    changed bullets to signals, changed removal so we don't have bullets bouncing around, unless we want that?

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,13 +2,19 @@
 extends RigidBody2D
 var player_origin
 
-func _fixed_process(delta):
-	for object in get_colliding_bodies():
-		if object.is_in_group('players') and object.get_name() != player_origin:
-			object.add_ammo(-20)
-		queue_free()
+#func _fixed_process(delta):
+#	for object in get_colliding_bodies():
+#		if object.is_in_group('players') and object.get_name() != player_origin:
+#			object.add_ammo(-20)
+#		queue_free()
+
+func bullet_hit(body):
+	if body.is_in_group('players') and body.get_name() != player_origin:
+		body.add_ammo(-20)
+	self.remove_and_skip()
 
 func _ready():
-	set_fixed_process(true)
+	connect("body_enter", self, "bullet_hit")
+	#set_fixed_process(true)
 
 
